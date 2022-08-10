3f6a6d4291504eeb1436e41bc084e5e0130f2ab8
Esther Ogundijo <sheyiogundijo@gmail.com>
2022-08-10 20:25:39 +0100

    updates app.ts

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,7 @@ const startApp = async () => {
   const errorRef: any = document.querySelector("#error");
   const nextButton = document.querySelector("#next") as HTMLButtonElement;
   const prevButton = document.querySelector("#prev") as HTMLButtonElement;
+  const pageview = document.querySelector("#pageview") as HTMLLabelElement;
 
   prevButton.disabled = true;
   errorRef.innerHTML = "";
@@ -29,11 +30,13 @@ const startApp = async () => {
 
     prevButton.disabled = false;
 
-    nextButton.innerHTML = "Loading...";
     const nextPage = state.currentPage + 1;
+
     if (state.availableData.hasOwnProperty(nextPage)) {
       // if data is available for prev fetching
       displayTableData(state.availableData[`${nextPage}`], tableRef);
+      pageview.dataset.pageview = `${nextPage}`;
+      pageview.innerHTML = `Showing Page ${nextPage}`;
     } else {
       await fetchData(
         `https://randomapi.com/api/8csrgnjw?key=LEIX-GF3O-AG7I-6J84&page=${nextPage}`,
@@ -41,6 +44,8 @@ const startApp = async () => {
           const [data] = response;
           state.availableData = data;
           displayTableData(data[`${nextPage}`], tableRef);
+          pageview.dataset.pageview = `${nextPage}`;
+          pageview.innerHTML = `Showing Page ${nextPage}`;
         },
         (error) => {
           errorRef.style.color = "red";
@@ -71,7 +76,6 @@ const startApp = async () => {
       return;
     }
 
-    prevButton.innerHTML = "Loading...";
     const prevPage = state.currentPage - 1;
 
     await fetchData(
@@ -82,6 +86,8 @@ const startApp = async () => {
         const [data] = response;
         state.availableData = data;
         displayTableData(data[`${prevPage}`], tableRef);
+        pageview.dataset.pageview = `${prevPage}`;
+        pageview.innerHTML = `Showing Page ${prevPage}`;
       },
       (error) => {
         errorRef.style.color = "red";
@@ -110,6 +116,8 @@ const startApp = async () => {
       state.currentPage = 1;
       state.availableData = data;
       displayTableData(data["1"], tableRef, true);
+      pageview.dataset.pageview = "1";
+      pageview.innerHTML = "Showing Page 1";
     },
     (error) => {
       errorRef.style.color = "red";
