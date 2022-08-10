d5c3efabd429a5e1216c2516414cd473bbefbc36
Esther Ogundijo <sheyiogundijo@gmail.com>
2022-08-10 20:07:16 +0100

    updates app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import displayTableData from "./helpers/display-table";
 
 const startApp = async () => {
   const state = {
-    currentPage: 0,
+    currentPage: 1,
     availableData: {
       paging: {},
     },
@@ -12,9 +12,10 @@ const startApp = async () => {
   // Get a reference to the table, error and buttons
   const tableRef: any = document.querySelector("#pag-table > tbody");
   const errorRef: any = document.querySelector("#error");
-  const nextButton = document.querySelector("#next");
-  const prevButton = document.querySelector("#prev");
+  const nextButton = document.querySelector("#next") as HTMLButtonElement;
+  const prevButton = document.querySelector("#prev") as HTMLButtonElement;
 
+  prevButton.disabled = true;
   errorRef.innerHTML = "";
 
   /**
@@ -22,10 +23,11 @@ const startApp = async () => {
    */
   nextButton?.addEventListener("click", async () => {
     if (!state.availableData.paging.hasOwnProperty("next")) {
-      console.log("no next");
-      //TODO: Disable next button
+      nextButton.disabled = true;
       return;
     }
+
+    prevButton.disabled = false;
 
     nextButton.innerHTML = "Loading...";
     const nextPage = state.currentPage + 1;
@@ -44,45 +46,61 @@ const startApp = async () => {
           errorRef.style.color = "red";
           errorRef.style.marginBottom = "1em";
           errorRef.innerHTML = error;
+        },
+        () => {
+          nextButton.innerHTML = "Loading...";
+          prevButton.disabled = true;
+          nextButton.disabled = true;
+        },
+        () => {
+          nextButton.innerHTML = "Next";
+          prevButton.disabled = false;
+          nextButton.disabled = false;
         }
       );
     }
     state.currentPage++;
-    nextButton.innerHTML = "Next";
   });
 
   /**
    * onClick of prev button
    */
   prevButton?.addEventListener("click", async () => {
-    if (!state.availableData.paging.hasOwnProperty("previous")) {
-      console.log("no prev!!!");
-      //TODO: Disable prev button
+    if (state.currentPage === 1) {
+      prevButton.disabled = true;
       return;
     }
 
     prevButton.innerHTML = "Loading...";
     const prevPage = state.currentPage - 1;
-    if (state.availableData.hasOwnProperty(prevPage)) {
-      // if data is available for prev fetching
-      displayTableData(state.availableData[`${prevPage}`], tableRef);
-    } else {
-      await fetchData(
-        `https://randomapi.com/api/8csrgnjw?key=LEIX-GF3O-AG7I-6J84&page=${prevPage}`,
-        (response) => {
-          const [data] = response;
-          state.availableData = data;
-          displayTableData(data[`${prevPage}`], tableRef);
-        },
-        (error) => {
-          errorRef.style.color = "red";
-          errorRef.style.marginBottom = "1em";
-          errorRef.innerHTML = error;
-        }
-      );
-    }
+
+    await fetchData(
+      `https://randomapi.com/api/8csrgnjw?key=LEIX-GF3O-AG7I-6J84&page=${prevPage}`,
+      (response) => {
+        prevButton.disabled = false;
+        nextButton.disabled = false;
+        const [data] = response;
+        state.availableData = data;
+        displayTableData(data[`${prevPage}`], tableRef);
+      },
+      (error) => {
+        errorRef.style.color = "red";
+        errorRef.style.marginBottom = "1em";
+        errorRef.innerHTML = error;
+      },
+      () => {
+        prevButton.innerHTML = "Loading...";
+        prevButton.disabled = true;
+        nextButton.disabled = true;
+      },
+      () => {
+        prevButton.innerHTML = "Previous";
+        prevButton.disabled = false;
+        nextButton.disabled = false;
+      }
+    );
+
     state.currentPage--;
-    prevButton.innerHTML = "Previous";
   });
 
   await fetchData(
@@ -97,6 +115,20 @@ const startApp = async () => {
       errorRef.style.color = "red";
       errorRef.style.marginBottom = "1em";
       errorRef.innerHTML = error;
+    },
+    () => {
+      nextButton.innerHTML = "Loading...";
+      prevButton.disabled = true;
+      nextButton.disabled = true;
+    },
+    () => {
+      nextButton.innerHTML = "Next";
+      nextButton.disabled = false;
+      if (state.currentPage === 1) {
+        prevButton.disabled = true;
+      } else {
+        prevButton.disabled = false;
+      }
     }
   );
 };
